bf390a083c9bfe473a6e6d983ef28fe049178b4b
weeast1521 <101035453+weeast1521@users.noreply.github.com>
2024-08-26 04:24:44 +0900

    GameView 추가

---
+++ untitled/src/pro1/view/GameView.java
@@ -0,0 +1,11 @@
+package pro1.view;
+
+import java.util.Scanner;
+
+public class GameView {
+    Scanner sc = new Scanner(System.in);
+
+    public int getGameSet(){
+        return sc.nextInt();
+    }
+}
