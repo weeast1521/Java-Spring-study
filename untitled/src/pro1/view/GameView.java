ceab861e6d508616932dfce8956e9b56f103d12d
weeast1521 <101035453+weeast1521@users.noreply.github.com>
2024-09-07 16:23:49 +0900

    StrikeAndBallCheck에 있던strike, ball의 갯수 print 기능을 view로 이동시킴

--- untitled/src/pro1/view/GameView.java
+++ untitled/src/pro1/view/GameView.java
@@ -8,4 +8,19 @@ public class GameView {
     public int getGameSet(){
         return sc.nextInt();
     }
+
+    public void printStrikeAndBallCount(int strike, int ball){
+        if (strike == 0 && ball == 0){
+            Message.NOT_BALL_NOT_STRIKE.println();
+        }
+        else if(strike == 0 && ball > 0){
+            Message.BALL.printBall(ball);
+        }
+        else if(strike > 0 && ball == 0){
+            Message.STRIKE.printStrike(strike);
+        }
+        else{
+            Message.BALL_STRIKE.printBallAndStrike(ball, strike);
+        }
+    }
 }
